82cf9e7537e3c42b94c2debcb06648cf950ac74d
ivy <oumaiymafrikel@gmail.com>
2023-06-20 06:31:29 -0700

    110

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,51 +1,34 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
-* print_to_98 - prints all natural numbers from n to 98,
-* followed by a new line
-* @n: print from this number
-* print_number function helper
-*/
-void print_number(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
-}
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
+ */
 
 void print_to_98(int n)
 {
-	int i;
-
+	int i, j;
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			print_number(i);
 			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
 	}
-	else
+	else if (n >= 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (j = n; j >= 98; j--)
 		{
-			print_number(i);
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
-	putchar('\n');
 }
+
