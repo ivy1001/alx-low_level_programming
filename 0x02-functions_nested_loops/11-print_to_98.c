fd9d81d834c6cb4eab104fbe5baf6f9d38a93915
ivy <oumaiymafrikel@gmail.com>
2023-06-20 05:43:15 -0700

    1110

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+#include <stdio.h>
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			print_number(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			print_number(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+}
