a87af6c8b29b28309ffe8af6d9e49152b003d0dd
ivy <oumaiymafrikel@gmail.com>
2023-07-05 12:15:48 -0700

    bonus

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+
+/**
+ * is_palindrome_helper - check if a string is a palindrome.
+ * @s: The string to check.
+ * @start: The starting index of the string.
+ * @end: The ending index of the string.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+
+	if (s[start] != s[end])
+		return (0);
+
+	return (is_palindrome_helper(s, start + 1, end - 1));
+}
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The string to calculate its length.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to check.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+	int length = _strlen(s);
+
+	return (is_palindrome_helper(s, 0, length - 1));
+}
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,23 +1,4 @@
 #include "main.h"
-
-/**
- * _sqrt_recursion - Returns the natural square root of a number.
- * @n: The number to calculate its square root.
- *
- * Return: The square root of n,
- *         or -1 if n does not have a natural square root.
- */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return -1;
-
-	if (n == 0 || n == 1)
-		return (n);
-
-	return (sqrt_helper(n, 1, n));
-}
-
 /**
  * sqrt_helper - Recursive helper function to find the square root of a number.
  * @n: The number to calculate its square root.
@@ -39,6 +20,25 @@ int sqrt_helper(int n, int start, int end)
 			return (sqrt_helper(n, start, mid - 1));
 		return (sqrt_helper(n, mid + 1, end));
 	}
-
 	return (end);
 }
+
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to calculate its square root.
+ *
+ * Return: The square root of n,
+ *         or -1 if n does not have a natural square root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 0 || n == 1)
+		return (n);
+
+	return (sqrt_helper(n, 1, n));
+}
+
